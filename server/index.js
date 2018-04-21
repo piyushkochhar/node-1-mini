843dcc9978e2d28ffc594289133a93e3952a64ec
piyushkochhar <piyush.kochhar1@gmail.com>
2018-04-21 18:49:25 -0500

    Passed all tests

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const bc = require('./controllers/books_controller.js');
+
+const app = express();
+
+app.use( bodyParser.json() );
+
+app.get('/api/books', bc.read);
+app.post('/api/books', bc.create);
+app.put('/api/books/:id', bc.update)
+app.delete('/api/books/:id', bc.delete)
+
+const port = 3000;
+app.listen( port, () => { console.log(`Server listening on port ${port}`); } );
